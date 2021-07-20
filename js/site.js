a6ec628b4027106f726ed97b7d9f857f83925414
ehartjen <67296112+ehartjen@users.noreply.github.com>
2021-07-20 15:20:09 -0400

    1

--- js/site.js
+++ js/site.js
@@ -1,1 +1,13 @@
 //this is a comment
+//get user data
+function submitdata() {
+
+    let loanamount = document.getElementById("loanamount").value
+    let payments = document.getElementById("payments").value
+    let rate = document.getElementById("rate").value
+}
+
+function totalmonthlypayment(loanamount, payments, rate) {
+    let totalmonthlypayment =
+
+}
